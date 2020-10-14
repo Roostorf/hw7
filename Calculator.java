7a27aabae7991257356e89d02a2a0cef0e4a9576
Constantine Peros <cperos@hawaii.edu>
2020-10-14 13:41:42 -1000

    Finished Extra Credit part, made everything into methods to be reused in PreFixCalc.

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,7 @@
 package edu.ics211.h07;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Stack;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -45,67 +47,86 @@ public class Calculator implements ICalculator {
     Stack<Number> stack = new Stack<Number>();  // create a stack
     String[] token =  expression.split(" ");    // split string into tolkens by split by space 
     System.out.println("token array length :" + token.length);
-    if (token.length > 1) {
-      if (!NumberUtils.isParsable(token[1])) { 
-        System.out.println("error 1 Second digit in string is non numeric");
-        throw new InvalidExpressionException();      
-      }
-    }
+    
+    precheckErrors(token);  // If its one and not a number error
+    
+
     if (token.length == 1 && NumberUtils.isParsable(token[0])) // If the array is only one, and it is a number
-    {
-      if (token[0].contains("."))  //If double
-      {
-        return Double.parseDouble(token[0]); // return numeric double value
-      } else {      
-        return Integer.parseInt(token[0]); // else return numeric int   
-      }
-    } else if (token.length > 1 
-        && (ArrayUtils.contains(token, "+")
-            || ArrayUtils.contains(token, "-")
-            || ArrayUtils.contains(token, "/")
-            || ArrayUtils.contains(token, "*"))) { // If the array is longer than one and contains an operator    
+    { 
+      return oneNum(token);
+
+    } else if (token.length > 1) { // If the array is longer than one and contains an operator    
+      
       for (int i = 0; i < token.length; i ++)
       {
         System.out.println("token value is: " + token[i]);  
         // if token is operator 
-        if ((token[i].equals("+") 
-            || token[i].equals("-") 
-            || token[i].equals("*") 
-            || token[i].equals("/"))) {
-          // if element is an operator take out last two numbers from stack and perform operation
+        if (isOperator(token, i)) {
           System.out.println("contains operator!");
-          performOperation(token[i], stack);
-        } 
-        // If token is numeric
-        if (NumberUtils.isParsable(token[i])) {
-          // if element is a number try adding to stack
-          
-          if (token[i].contains("."))  //If double
-          {
-            stack.push(Double.parseDouble(token[i]));
-            System.out.println("Double " + token[i] + " added to the number stack");
-
-          } else {
-            
-            try {
-              stack.push(Integer.parseInt(token[i]));
-              System.out.println("Integer " + token[i] + " added to the number stack");
-              
-            } catch (NumberFormatException nfe)
-            {
-              throw new NumberFormatException();
-            }  
-          }   
+          performOperation(token[i], stack); 
         }
+        isNumeric(stack, token, i);
+
              
       }
     } else {
-      System.out.println("error 2 Contains invalid char or no Operator ");
+      System.out.println("error 2 Contains invalid char or no Operator "); //shouldnt happen becuase pretests
       throw new InvalidExpressionException();
     }  
     return stack.pop();
   }
 
+
+  /**
+   * Tells us if operator.
+   * @param token token array.
+   * @param i is the iterator.
+   * @return Boolean, true if operator.
+   */
+  private boolean isOperator(String[] token, int i) {
+
+    if ((token[i].equals("+") 
+        || token[i].equals("-") 
+        || token[i].equals("*") 
+        || token[i].equals("/"))) {
+      // if element is an operator true
+      return true;
+    } else {
+      return false; // otherwise false
+    }
+    
+  }
+
+  /**
+   * if it is numeric push to the stack.
+   * @param stack  The stack
+   * @param token the token array
+   * @param i the iterator from for loop
+   */
+  private void isNumeric(Stack<Number> stack, String[] token, int i) {
+    // If token is numeric
+    if (NumberUtils.isParsable(token[i])) {
+      // if element is a number try adding to stack
+      
+      if (token[i].contains("."))  //If double
+      {
+        stack.push(Double.parseDouble(token[i]));
+        System.out.println("Double " + token[i] + " added to the number stack");
+
+      } else {
+        
+        try {
+          stack.push(Integer.parseInt(token[i]));
+          System.out.println("Integer " + token[i] + " added to the number stack");
+          
+        } catch (NumberFormatException nfe)
+        {
+          throw new NumberFormatException();
+        }  
+      }   
+    }
+    
+  }
 
   /**
    * Performs an operation when encountering operator.
@@ -209,76 +230,137 @@ public class Calculator implements ICalculator {
 
   @Override
   public Number preFixCalculate(String expression) throws InvalidExpressionException {
-
-    Stack<String> opStack = new Stack<String>(); // create a stack for operators
+    
+    Stack<Number> stack = new Stack<Number>();  // create a stack
     String[] token =  expression.split(" ");    // split string into tolkens by split by space 
     System.out.println("token array length :" + token.length);
     
+    System.out.println("Token array was ");
+    printArr(token);
+    
+    token = runItBackwards(token); // makes array backwards
+    
+    System.out.println("Token array is now backwards as ");
+    printArr(token);
+    
+    precheckErrors(token);  // error if one and not a number or more than one and second value isn't an operator
+    
+    if (token.length == 1 && NumberUtils.isParsable(token[0])) // If the array is only one, and it is a number
+    { 
+      return oneNum(token);
+
+    } else if (token.length > 1) { // If the array is longer than one and contains an operator    
+      
+      for (int i = 0; i < token.length; i ++)
+      {
+        System.out.println("token value is: " + token[i]);  
+        // if token is operator 
+        if (isOperator(token, i)) {
+          System.out.println("contains operator!");
+          
+          stack = switchemup(stack);
+          
+          performOperation(token[i], stack); 
+        }
+        isNumeric(stack, token, i);
+
+             
+      }
+    } else {
+      System.out.println("error 2 Contains invalid char or no Operator ");
+      throw new InvalidExpressionException();
+    }  
+    return stack.pop();
+
+  }
+
+  /**
+   * Switches top two in stack in case of operation.
+   * @param stack the stack.
+   * @return the stack top two values switched.
+   */
+  private Stack<Number> switchemup(Stack<Number> stack) {
+    Number popA = stack.pop();
+    Number popB = stack.pop();
+    stack.push(popA);
+    stack.push(popB);
+    return stack;
+  }
+
+  /**
+   * prints the array.
+   * @param token token array
+   */
+  private void printArr(String[] token) { // prints the entire array
+    for (int i = 0; i < token.length; i++)
+    {
+      System.out.println(token[i]);
+    }  
+  }
+
+  /**
+   * returns a backwards string.
+   * @param token tokenizedArray.
+   * @return the same string backwards
+   */
+  private String[] runItBackwards(String[] token) { 
+ 
+    Collections.reverse(Arrays.asList(token));
+
+    return token;
+  }
+
+  /**
+   * what to do on a single digit. returns double if double, or int if int.
+   * @param token the token
+   * @return a single number, double or int depending on decimal or not
+   */
+  public Number oneNum(String[] token) {
+    if (token[0].contains("."))  //If double
+    {
+      return Double.parseDouble(token[0]); // return numeric double value
+    } else {      
+      return Integer.parseInt(token[0]); // else return numeric int   
+    }
+    
+    
+    
+  }
+
+  /**
+   * Precheck array for potential errors.
+   * @param token array of tokens
+   * @throws InvalidExpressionException  error to throw on precheck
+   */
+  private void precheckErrors(String[] token) throws InvalidExpressionException {
     if (token.length == 1 && !NumberUtils.isParsable(token[0])) // If the array is only one, and it isn't a number
     {
-      System.out.println("error A One Digit no Operator ");
+      System.out.println("error A One Digit Not a number ");
       throw new InvalidExpressionException();
       
     }
     
-    if (token.length == 1 && NumberUtils.isParsable(token[0])) // If the array is only one, and it is a number
-    {
-      if (token[0].contains("."))  //If double
-      {
-        return Double.parseDouble(token[0]); // return numeric double value
-      } else {      
-        return Integer.parseInt(token[0]); // else return numeric int   
-      }
-    } else if (token.length > 1 
-        && (ArrayUtils.contains(token, "+")
-            || ArrayUtils.contains(token, "-")
-            || ArrayUtils.contains(token, "/")
-            || ArrayUtils.contains(token, "*"))) { // If the array is longer than one and contains an operator
-      
-      for (int i = 0; i < token.length; i++) // do this for array length 
-      {
-        System.out.println("token value is: " + token[i]);  
-        // if token is operator 
-        if ((token[i].equals("+") 
-            || token[i].equals("-") 
-            || token[i].equals("*") 
-            || token[i].equals("/"))) {
-          // if element is an operator push it to the operator stack
-          System.out.println("contains operator! Pushing it to the opStack");
-          opStack.push(token[i]);
-        }
-        // if numeric/parsable put it in a variable
-        if (NumberUtils.isParsable(token[i])) {
-          if (token[i].contains("."))  //If double
-          {
-            double dblA = Double.parseDouble(token[i]);
-            System.out.println("Double " + dblA + " was foud");
-
-          } else {
-            try {
-              int intA = Integer.parseInt(token[i]);
-              System.out.println("Integer " + intA + " was found");
-              
-            } catch (NumberFormatException nfe)
-            {
-              throw new NumberFormatException();
-            }  
-          }   
-        
-          
-          
-        }
-        // if next is numeric do maths
-        
+    if (token.length > 1) {
+      if (!NumberUtils.isParsable(token[1])) { 
+        System.out.println("error 1 Second digit in string is non numeric");
+        throw new InvalidExpressionException();      
       }
       
-    } else {
-      System.out.println("error B Contains invalid char or no Operator ");
-      throw new InvalidExpressionException();
+      if (!ArrayUtils.contains(token, "+")    //If array is longer than 1 and doesnt contain any operators throw error
+          && !ArrayUtils.contains(token, "-")
+          && !ArrayUtils.contains(token, "/")
+          && !ArrayUtils.contains(token, "*")) {
+        
+        System.out.println("error B Array contains multiple values but no operators");
+        throw new InvalidExpressionException(); 
+            
+      }
+      
       
     }
-
-    return 0;
+    
+    
+    
   }
 
 }
